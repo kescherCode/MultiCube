94f2b8b8214d42bc710c5bddf8acfdf70424f01a
Jeremy Kescher <filthycoding@users.noreply.github.com>
2018-05-17 10:17:49 +0200

    Removed redundant code

--- MultiCube/Program.cs
+++ MultiCube/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 using System.Collections.Generic;
 using System.Text;
@@ -60,28 +60,6 @@ namespace MultiCube
     }
     class Program
     {
-
-        internal static class DllImports
-        {
-            [StructLayout(LayoutKind.Sequential)]
-            public struct Coords
-            {
-                public short X;
-                public short Y;
-                public Coords(short x, short y)
-                {
-                    X = x;
-                    Y = y;
-                }
-
-            }
-            [DllImport("kernel32.dll")]
-            public static extern IntPtr GetStdHandle(int handle);
-            public const int CONSOLE = -11;
-            [DllImport("kernel32.dll", SetLastError = true)]
-            public static extern bool SetConsoleDisplayMode(IntPtr ConsoleOutput, uint Flags, out Coords NewScreenBufferDimensions);
-        }
-
         const int LEDGE_LENGTH = 25;
         static IEnumerable<CornerData> lines;
         static ConsoleKeyInfo keyPress;
@@ -234,100 +212,6 @@ namespace MultiCube
                 Console.Clear();
                 Console.CursorVisible = false; // Workaround for cursor being visible if you click into the window
             }
-        }
-    }
-
-
-    class Point3D
-    {
-        public float x;
-        public float y;
-        public float z;
-
-        public Point3D(float X, float Y, float Z)
-        {
-            x = X;
-            y = Y;
-            z = Z;
-        }
-
-        public Point3D RotateX(float angle)
-        {
-            float rad = angle * (float)Math.PI / 180;
-
-            float cosa = (float)Math.Cos(rad);
-            float sina = (float)Math.Sin(rad);
-
-            float oldY = y;
-            // New Y and Z axis'
-            y = y * cosa - z * sina;
-            z = oldY * sina + z * cosa;
-            return this;
-        }
-
-        public Point3D RotateY(float angle)
-        {
-            float rad = angle * (float)Math.PI / 180;
-
-            float cosa = (float)Math.Cos(rad);
-            float sina = (float)Math.Sin(rad);
-
-            float oldX = x;
-            // New X and Z axis'
-            x = z * sina + x * cosa;
-            z = z * cosa - oldX * sina;
-            return this;
-        }
-
-        public Point3D RotateZ(float angle)
-        {
-            float rad = angle * (float)Math.PI / 180;
-
-            float cosa = (float)Math.Cos(rad);
-            float sina = (float)Math.Sin(rad);
-
-            float oldX = x;
-            // New X and Y axis'
-            x = x * cosa - y * sina;
-            y = y * cosa + oldX * sina;
-            return this;
-        }
-
-        // Project the current Point into 2D plotted space using X and Y axis'
-        public Point3D Project(float width, float height, float fov, float viewDist)
-        {
-            float factor = fov / (viewDist + z);
-            Point3D p = new Point3D(x, y, z);
-            return new Point3D(p.x * factor + width / 2, -p.y * factor + height / 2, 1);
-        }
-
-        // Returns the sum of all coordinates squared.
-        public float Length { get { return (float)Math.Sqrt(x * x + y * y + z * z); } }
-        public static Point3D operator *(float scale, Point3D x)
-        {
-            Point3D p = new Point3D(x.x, x.y, x.z);
-            p.x *= scale;
-            p.y *= scale;
-            p.z *= scale;
-            return p;
-        }
-
-        public static Point3D operator -(Point3D left, Point3D right)
-        {
-            Point3D p = new Point3D(left.x, left.y, left.z);
-            p.x -= right.x;
-            p.y -= right.y;
-            p.z -= right.z;
-            return p;
-        }
-
-        public static Point3D operator +(Point3D left, Point3D right)
-        {
-            Point3D p = new Point3D(left.x, left.y, left.z);
-            p.x += right.x;
-            p.y += right.y;
-            p.z += right.z;
-            return p;
         }
     }
 }
