b8e6fbe26d3fe4c1840bdd76e005d4544b2b3f75
Jeremy Kescher <jeremy@kescher.at>
2019-05-02 09:25:34 +0200

    idk

--- build.cmd
+++ build.cmd
@@ -4,4 +4,3 @@ dotnet publish -c Release -r win-x86 -o bin/publish/win-x86
 dotnet publish -c Release -r win-x64 -o bin/publish/win-x64
 dotnet publish -c Release -r osx-x64 -o bin/publish/osx-x64
 dotnet publish -c Release -r linux-x64 -o bin/publish/linux-x64
-
