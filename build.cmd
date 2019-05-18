e8208bc1c370f3319249fee9abdf16f8d2ee1c5d
Jeremy Kescher <jeremy@kescher.at>
2019-05-18 11:46:30 +0200

    warp-packer now should work

--- build.cmd
+++ build.cmd
@@ -1,7 +1,10 @@
 cd MultiCube
+mkdir bin
+mkdir bin\publish
 dotnet publish -c Release -r win-x64 -o bin/publish/win-x64
 ..\warp-packer --arch windows-x64 --input_dir bin/publish/win-x64 --exec MultiCube.exe --output bin/publish/MultiCube-win-x64.exe
 dotnet publish -c Release -r osx-x64 -o bin/publish/osx-x64
-..\warp-packer --arch osx-x64 --input_dir bin/publish/osx-x64 --exec MultiCube --output bin/publish/MultiCube-osx-64
+..\warp-packer --arch macos-x64 --input_dir bin/publish/osx-x64 --exec MultiCube --output bin/publish/MultiCube-osx-64
 dotnet publish -c Release -r linux-x64 -o bin/publish/linux-x64
 ..\warp-packer --arch linux-x64 --input_dir bin/publish/linux-x64 --exec MultiCube --output bin/publish/MultiCube-linux-x64
+cd bin/publish && for /f "delims=" %%d in ('dir /ad /b') do @rd /q /s "%%d"
--- build.sh
+++ build.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env sh
 
 cd MultiCube
+mkdir bin
+mkdir bin/publish
 dotnet publish -c Release -r win-x64 -o bin/publish/win-x64
 ../warp-packer --arch windows-x64 --input_dir bin/publish/win-x64 --exec MultiCube.exe --output bin/publish/MultiCube-win-x64.exe &
 dotnet publish -c Release -r osx-x64 -o bin/publish/osx-x64
-../warp-packer --arch osx-x64 --input_dir bin/publish/osx-x64 --exec MultiCube --output bin/publish/MultiCube-osx-64 &
+../warp-packer --arch macos-x64 --input_dir bin/publish/osx-x64 --exec MultiCube --output bin/publish/MultiCube-osx-64 &
 dotnet publish -c Release -r linux-x64 -o bin/publish/linux-x64
 ../warp-packer --arch linux-x64 --input_dir bin/publish/linux-x64 --exec MultiCube --output bin/publish/MultiCube-linux-x64 &
 wait
+cd bin/publish && (for f in $(find . ! -path . -type d)
+do
+	rm -rf "$f"
+done)
